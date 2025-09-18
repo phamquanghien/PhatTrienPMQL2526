7e91209116e7fadebaeef56dd236cf0c19db3228
phamquanghien <pqh.cntt@gmail.com>
2025-09-18 10:31:12 +0700

    Add_Package

--- DemoMVC/Models/Entities/Person.cs
+++ DemoMVC/Models/Entities/Person.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DemoMVC.Models.Entities
 {
     public class Person
     {
+        [Key]
         public string StudentID { get; set; } = default!;
         public string FullName { get; set; } = default!;
     }
